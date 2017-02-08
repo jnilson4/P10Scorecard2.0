a567aa5af5e0aa6198cdcc8edc7cdb66f92352db
Jake Nilson <j.nilson132@gmail.com>
2017-02-08 12:17:29 -0700

    Added the elements to the ManualOverride panel.

--- src/score/view/ManualOverride.java
+++ src/score/view/ManualOverride.java
@@ -14,7 +14,9 @@ public class ManualOverride extends JPanel
 	private ImageIcon backgroundImage, helpButton;
 	private JLabel imageText, imageText1;
 	
+	private JLabel player1, player2, player3, player4, player5, player6, player7, player8;
 	private JTextField player1Points, player2Points, player3Points, player4Points, player5Points, player6Points, player7Points, player8Points;
+	private JTextField player1Phase, player2Phase, player3Phase, player4Phase, player5Phase, player6Phase, player7Phase, player8Phase;
 	
 	private JButton returnToScore, submitChanges;
 	
@@ -32,6 +34,34 @@ public class ManualOverride extends JPanel
 		this.returnToScore = new JButton("Return to Scorecard");
 		this.submitChanges = new JButton("Submit Changes");
 		
+		String [] playerNames = baseController.getNames();
+		this.player1 = new JLabel(playerNames[0]);
+		this.player2 = new JLabel(playerNames[1]);
+		this.player3 = new JLabel(playerNames[2]);
+		this.player4 = new JLabel(playerNames[3]);
+		this.player5 = new JLabel(playerNames[4]);
+		this.player6 = new JLabel(playerNames[5]);
+		this.player7 = new JLabel(playerNames[6]);
+		this.player8 = new JLabel(playerNames[7]);
+		
+		this.player1Points = new JTextField(4);
+		this.player2Points = new JTextField(4);
+		this.player3Points = new JTextField(4);
+		this.player4Points = new JTextField(4);
+		this.player5Points = new JTextField(4);
+		this.player6Points = new JTextField(4);
+		this.player7Points = new JTextField(4);
+		this.player8Points = new JTextField(4);
+		
+		this.player1Phase = new JTextField(4);
+		this.player2Phase = new JTextField(4);
+		this.player3Phase = new JTextField(4);
+		this.player4Phase = new JTextField(4);
+		this.player5Phase = new JTextField(4);
+		this.player6Phase = new JTextField(4);
+		this.player7Phase = new JTextField(4);
+		this.player8Phase = new JTextField(4);
+
 		setupPanel();
 		setupGUI();
 		setupLayout();
