332d662e172249d1fb03784db2c3ca3efa9b6700
Jake Nilson <j.nilson132@gmail.com>
2017-02-08 12:10:55 -0700

    Added the helpButton to the view.

--- src/score/view/ManualOverride.java
+++ src/score/view/ManualOverride.java
@@ -11,8 +11,10 @@ public class ManualOverride extends JPanel
 	private ScoreController baseController;
 	private SpringLayout baseLayout;
 	
-	private ImageIcon backgroundImage;
-	private JLabel imageText;
+	private ImageIcon backgroundImage, helpButton;
+	private JLabel imageText, imageText1;
+	
+	private JTextField player1Points, player2Points, player3Points, player4Points, player5Points, player6Points, player7Points, player8Points;
 	
 	private JButton returnToScore, submitChanges;
 	
@@ -23,6 +25,9 @@ public class ManualOverride extends JPanel
 		
 		this.backgroundImage = new ImageIcon(getClass().getResource("/score/view/images/p10bg.jpg"));
 		this.imageText = new JLabel(" ", backgroundImage, JLabel.CENTER);
+		
+		this.helpButton = new ImageIcon(getClass().getResource("/score/view/images/questionMark2.png"));
+		this.imageText1 = new JLabel(" ", helpButton, JLabel.CENTER);
 		
 		this.returnToScore = new JButton("Return to Scorecard");
 		this.submitChanges = new JButton("Submit Changes");
@@ -42,6 +47,8 @@ public class ManualOverride extends JPanel
 		this.add(returnToScore);
 		this.add(submitChanges);
 		
+		this.add(imageText1);
+		
 		this.add(imageText);
 	}
 	
@@ -56,6 +63,8 @@ public class ManualOverride extends JPanel
 		baseLayout.putConstraint(SpringLayout.SOUTH, returnToScore, -10, SpringLayout.SOUTH, this);
 		baseLayout.putConstraint(SpringLayout.NORTH, submitChanges, 0, SpringLayout.NORTH, returnToScore);
 		baseLayout.putConstraint(SpringLayout.EAST, submitChanges, -10, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, imageText1, 10, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, imageText1, -10, SpringLayout.EAST, this);
 	}
 	
 	private void setupListeners()
@@ -67,6 +76,18 @@ public class ManualOverride extends JPanel
 				ScorePanel score = new ScorePanel(baseController);
 				baseController.getFrame().getContentPane().removeAll();
 				baseController.getFrame().setContentPane(score);
+				baseController.getFrame().validate();
+				repaint();
+			}
+		});
+		
+		imageText1.addMouseListener(new MouseAdapter()
+		{
+			public void mouseClicked(MouseEvent clicked)
+			{
+				InstructionPanel help = new InstructionPanel(baseController);
+				baseController.getFrame().getContentPane().removeAll();
+				baseController.getFrame().setContentPane(help);
 				baseController.getFrame().validate();
 				repaint();
 			}
